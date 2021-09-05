14d56623e8a2921d5899c0c5cb03e8d34fd2a370
Shaunak <shaunakd01@gmail.com>
2021-09-05 16:55:53 +0530

    login functionality added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask 
 from flask import render_template
 from flask import request
-
+from flask import flash
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 app = Flask(__name__ )
 
 @app.route("/")
@@ -9,10 +11,9 @@ def index():
     return render_template('index.html')
 
 @app.route("/login" , methods=["GET" , "POST"])
-@app.route('/')
 def loginHandler():
-    if request.method=="GET":
-        return render_template('login.html')
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+    if request.method == 'POST':
+        # print(request.form['email'] , request.form['password'])
+        # if request.form['email'] == 'test123@testmail.com' or request.form['password'] == 'admin':
+        return ("<p>Successful login</p>")
+    return render_template('login.html')
